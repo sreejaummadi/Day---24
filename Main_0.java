e9095fbe48a0c59e1433760f3d0e51d356f8c883
Ummadi Sreeja <157631852+sreejaummadi@users.noreply.github.com>
2025-09-25 12:13:00 +0530

    Encapsulation Basic Example

---
+++ Main_0.java
@@ -0,0 +1,12 @@
+package com.codegnan.oopexamples;
+
+public class Main_0 {
+
+	public static void main(String[] args) {
+		Human human1=new Human();
+		human1.setName("Ramu");
+		human1.setAge(20);
+		System.out.println("Name: "+human1.getName());
+		System.out.println("Age : "+human1.getAge());	
+	}
+}
