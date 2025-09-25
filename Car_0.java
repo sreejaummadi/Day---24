2a7b45dfdc3727ed57b0369252afd7d092063af8
Ummadi Sreeja <157631852+sreejaummadi@users.noreply.github.com>
2025-09-25 12:15:18 +0530

    Encapsulation car Example

---
+++ Car_0.java
@@ -0,0 +1,19 @@
+package com.codegnan.oopexamples;
+
+public class Car_0 {
+	private String car_model;
+	private int year;
+	public String getCar_model() {
+		return car_model;
+	}
+	public void setCar_model(String car_model) {
+		this.car_model = car_model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
---
+++ Main_01.java
@@ -0,0 +1,13 @@
+package com.codegnan.oopexamples;
+
+public class Main_01 {
+
+	public static void main(String[] args) {
+		Car_0 car1=new Car_0();
+		car1.setCar_model("Camry");
+		car1.setYear(2000);
+		System.out.println("Car Model : "+car1.getCar_model()+" Manufacture Year: "+car1.getYear());
+
+	}
+
+}
